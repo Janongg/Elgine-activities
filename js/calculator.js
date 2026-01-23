e6a6f225cfe557248a8f28e55a88791d8efb329c
Janongg <elgine.aldaca@gmail.com>
2026-01-23 11:38:24 +0800

    Delete js directory

--- js/calculator.js
+++
@@ -1,9 +0,0 @@
-function press(val) {
-    var display = document.getElementById('screen');
-    display.value = display.value + val;
-}
-
-function allClear() {
-    var display = document.getElementById('screen');
-    display.value = "";
-}
